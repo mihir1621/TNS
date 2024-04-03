056ddefd18b35204c2d4d80582f7361c7daa3b95
Mihir Chaudhari <mihirchaudhari1621@gmail.com>
2024-04-03 22:02:15 +0530

    Omkar added

--- TNS_Training/com/TNS/ScannerDemo/TestHello.java
+++ TNS_Training/com/TNS/ScannerDemo/TestHello.java
@@ -4,7 +4,8 @@ public class TestHello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello mihir");
+		System.out.println("Hello mihir");
+System.out.println("Hello Omakr");
 	}
 
 }
