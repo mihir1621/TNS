e139da9af0cb7c67b26c82564485a62346ea34bb
Mihir Chaudhari <mihirchaudhari1621@gmail.com>
2024-04-03 22:05:59 +0530

    for loop added

---
+++ TNS_Training/com/TNS/DemoForTraining/ForLoop.java
@@ -0,0 +1,13 @@
+package com.TNS.DemoForTraining;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+for(int i=1;i<=9;i++) {
+	System.out.println(a);
+}
+	}
+
+}
